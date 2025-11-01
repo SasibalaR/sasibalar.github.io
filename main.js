713646b95a677d815b73e1d40b455970b4752f90
Sasibala <Sasibala_rsv@yahoo.com>
2025-11-02 00:43:16 +0530

    n

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-function loadContent(url, event) {
-    event.preventDefault(); // Prevent default link behavior
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(html => {
-            document.getElementById('content-container').innerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error loading HTML:', error);
-            document.getElementById('content-container').innerHTML = '<p>Error loading content.</p>';
-        });
-}
+
+$(document).ready(function() {
+    $('.nav-link').on('click', function(e) {
+        e.preventDefault();
+
+        var pageUrl = $(this).attr('href');
+
+        $('#content-container').load(pageUrl);
+    });
+});
