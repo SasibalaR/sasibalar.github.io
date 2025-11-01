9b7d30c67204a4d49d1508b2e0603e66d845aadc
Sasibala <Sasibala_rsv@yahoo.com>
2025-11-02 00:35:07 +0530

    g

--- main.js
+++ main.js
@@ -1,37 +1,18 @@
-/*===== MENU SHOW Y HIDDEN =====*/ 
-const navMenu = document.getElementById('nav-menu'),
-      toggleMenu = document.getElementById('nav-toggle'),
-      closeMenu = document.getElementById('nav-close')
+function loadContent(url, event) {
+    event.preventDefault(); // Prevent default link behavior
 
-/*SHOW*/ 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
-
-/*HIDDEN*/
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
-
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
-
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  navMenu.classList.remove('show')
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById('content-container').innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading HTML:', error);
+            document.getElementById('content-container').innerHTML = '<p>Error loading content.</p>';
+        });
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-$(document).ready(function() {
-    $('.nav-link').on('click', function(e) {
-        e.preventDefault();
-
-        var pageUrl = $(this).attr('href');
-
-        $('#content-container').load(pageUrl);
-    });
-});
