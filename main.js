330305067925719ed94dde46e0aac883c796ce94
Sasibala <Sasibala_rsv@yahoo.com>
2025-11-02 00:25:14 +0530

    menu click

--- main.js
+++ main.js
@@ -26,20 +26,12 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-function loadContent(url) {
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.text(); // Get the response as plain text (HTML)
-            })
-            .then(html => {
-                document.getElementById('content-container').innerHTML = html; // Insert the fetched HTML
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-                document.getElementById('content-container').innerHTML = '<p>Error loading content.</p>';
-            });
-    }
-    
+$(document).ready(function() {
+    $('.nav-link').on('click', function(e) {
+        e.preventDefault();
+
+        var pageUrl = $(this).attr('href');
+
+        $('#content-container').load(pageUrl);
+    });
+});
